8f6d997ecade12a8d6538f23e29bac95e6c10c39
Sergej V <vasiliokas@gmail.com>
2021-10-14 00:13:52 +0100

    load data to list

--- 01-AppMovieList/src/containers/List.js
+++ 01-AppMovieList/src/containers/List.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import Card from '../components/Card/Card.js';
+import testData from './data.json';
 
 class List extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+        }
+    }
+
+    async componentDidMount() {
+        const movies = await fetch('./data.json');
+        const moviesJSON = await movies.json();
+        if (moviesJSON) {
+            this.setState({
+                data: moviesJSON,
+                loading: false,
+            });
+        }
+        
+    }
+                
     render() {
+        console.log(JSON.stringify(testData)); // works
+        
         return(
             <div>
                 <h1>Hello List</h1>
