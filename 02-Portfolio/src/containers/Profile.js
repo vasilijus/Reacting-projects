92679715fa711def03ed2806250b049e790f437c
Sergio <vasiliokas@gmail.com>
2021-10-25 23:48:39 +0100

    errors loging

--- 02-Portfolio/src/containers/Profile.js
+++ 02-Portfolio/src/containers/Profile.js
@@ -17,7 +17,7 @@ class Profile extends Component
     }
     
     async componentDidMount() {
-        
+
         try {
             const temp = '{"login":"vasilijus","id":6454612,"node_id":"MDQ6VXNlcjY0NTQ2MTI=","avatar_url":"https://avatars.githubusercontent.com/u/6454612?v=4","gravatar_id":"","url":"https://api.github.com/users/vasilijus","html_url":"https://github.com/vasilijus","followers_url":"https://api.github.com/users/vasilijus/followers","following_url":"https://api.github.com/users/vasilijus/following{/other_user}","gists_url":"https://api.github.com/users/vasilijus/gists{/gist_id}","starred_url":"https://api.github.com/users/vasilijus/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/vasilijus/subscriptions","organizations_url":"https://api.github.com/users/vasilijus/orgs","repos_url":"https://api.github.com/users/vasilijus/repos","events_url":"https://api.github.com/users/vasilijus/events{/privacy}","received_events_url":"https://api.github.com/users/vasilijus/received_events","type":"User","site_admin":false,"name":"Sergej Vasiljev","company":null,"blog":"","location":"Birmingham","email":null,"hireable":null,"bio":"Mobile and Web Developer","twitter_username":null,"public_repos":102,"public_gists":16,"followers":0,"following":0,"created_at":"2014-01-20T21:26:48Z","updated_at":"2021-10-24T20:29:52Z"}'
             const tempProfileJSON = JSON.parse(temp);
@@ -61,10 +61,10 @@ class Profile extends Component
     }
 
     render() {
-        const { data, loading, repositories } = this.state;
+        const { data, loading, repositories, error } = this.state;
 
-        if (loading) {
-            return <div>Loading</div>;
+        if (loading || error) {
+            return <div>{loading ? 'Loading...' : error}</div>;
         }
 
         const items = [
