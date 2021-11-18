75596e982bbce557ee3a40e93df3916fb0509025
Sergej <vasiliokas@gmail.com>
2021-11-18 21:29:34 +0000

    Question component Shell

---
+++ 04-SSR_Based-feed/src/components/Questions/Questions.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const QuestionWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    margin: 4%;
+`;
+
+const Alert = styled.div`
+    text-align: center;
+`;
+
+class Question extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+            error: '',
+        };
+    }
+
+    render() {
+        const { data, loading, error } = this.state;
+
+        if(loading || error) {
+            return <Alert>{ loading ? 'Loading...' : error }</Alert>;
+        }
+
+        return (
+            <QuestionWrapper></QuestionWrapper>
+        )
+    }
+}
+
+export default Question;
