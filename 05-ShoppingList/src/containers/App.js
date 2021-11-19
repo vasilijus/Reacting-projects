cc67164b14389792878cb67fc7104711785885d3
Sergej V <vasiliokas@gmail.com>
2021-11-19 23:55:05 +0000

    implement datacontext to app

--- 05-ShoppingList/src/containers/App.js
+++ 05-ShoppingList/src/containers/App.js
@@ -5,6 +5,7 @@ import Header from '../components/Header/Header';
 import Lists from './Lists';
 import List from './List';
 import Form from './Form';
+import ListsContextProvider, { ListsContext } from '../Context/ListsContextProvider';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -27,11 +28,18 @@ const App = () => (
     <GlobalStyle />
     <AppWrapper>
       <Header />
-      <Switch>
-        <Route exact path='/' component={Lists} />
-        <Route path='/list/:id/new' component={Form} />
-        <Route path='/list/:id' component={List} />
-      </Switch>
+      <ListsContextProvider>
+        <ListsContext.Consumer>
+          { ({ lists }) => (
+            <Switch>
+              <Route exact path='/' component={Lists} />
+              <Route path='/list/:id/new' component={Form} />
+              <Route path='/list/:id' component={List} />
+            </Switch>
+          )}
+        </ListsContext.Consumer>
+        
+      </ListsContextProvider>
     </AppWrapper>
   </>
 );
