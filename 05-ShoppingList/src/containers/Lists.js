c087c1d90a38213a46642f3a84bd8b84319544b2
Sergej V <vasiliokas@gmail.com>
2021-11-20 00:01:00 +0000

    move logic

--- 05-ShoppingList/src/containers/Lists.js
+++ 05-ShoppingList/src/containers/Lists.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import withDataFetching from '../withDataFetching';
+// import withDataFetching from '../withDataFetching';
 import SubHeader from '../components/Header/SubHeader';
 
 const ListWrapper = styled.div`
@@ -33,13 +33,14 @@ const Alert = styled.span`
   text-align: center;
 `;
 
-const Lists = ({ data, loading, error, history }) =>
+// const Lists = ({ data, loading, error, history }) =>
+const Lists = ({ lists, loading = false, error = false, match, history }) =>
   !loading && !error ? (
     <>
       {history && <SubHeader title='Your Lists' />}
       <ListWrapper>
-        {data &&
-          data.map(list => (
+        {/* {data && data.map(list => ( */}
+        {lists && lists.map(list => (
             <ListLink key={list.id} to={`list/${list.id}`}>
               <Title>{list.title}</Title>
             </ListLink>
@@ -50,7 +51,8 @@ const Lists = ({ data, loading, error, history }) =>
     <Alert>{loading ? 'Loading...' : error}</Alert>
   );
 
-export default withDataFetching({
-  dataSource:
-    'https://my-json-server.typicode.com/PacktPublishing/React-Projects/lists',
-})(Lists);
+// export default withDataFetching({
+//   dataSource:
+//     'https://my-json-server.typicode.com/PacktPublishing/React-Projects/lists',
+// })(Lists);
+export default Lists;
